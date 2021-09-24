6e07e913bfcc6eeee6064550b447b41ff48184e7
maliksagar96 <malik.sagar97@gmail.com>
2021-09-24 15:39:30 +0530

    Final Touches required

--- _8bit.ino
+++ _8bit.ino
@@ -1,9 +1,20 @@
-const int ledPin = 13;
-volatile byte state = LOW;
-int count = 0;
+int ledPin =  13;
+int count16bit = 0;
+int count8bit = 0;
+int hourCounter = 0;
 int secondCounter = 0;
-int count16 = 0;
-int count16B = 0;
+int secCounter = 0;
+
+bool ledOn = 0;
+bool ledOff = 1;
+bool twelvehrFlag = 0;      //Twelve hour Flag
+bool state = 0;
+bool timeEnd = 0;
+
+void initTimer2() {
+  TCCR2B |= (TCCR2B & B11111000) | 0x07;
+  TIMSK2 |= (TIMSK2 & B11111110) | 0x01;
+}
 
 void init16bit() {
   
@@ -16,35 +27,67 @@ void init16bit() {
   TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
 }
 
-void setup() {
-   Serial.begin(9600);
-   Serial.println("Setup completed");
-   noInterrupts();
-   pinMode(ledPin, OUTPUT);
-   TIMSK2 = (TIMSK2 & B11111110) | 0x01;      //Last bit of TIMSK2 is high to enable overflow interrupt
-   TCCR2B = (TCCR2B & B11111000) | 0x07;      //1024 prescalar
-   init16bit();
-   TCNT1 |= 0;
-   TCNT2 |= 0;
-   interrupts();
-}
- 
-void loop() {
+ISR(TIMER2_OVF_vect){
+  count8bit++;
+  if(count8bit >= 30) {
+   secCounter++;
+   count8bit = 0;
+  }
   
-  digitalWrite(ledPin, state);
+  if((secCounter == 1) && (ledOff)) {
+    state = 1;
+    ledOn = 1;
+    ledOff = 0;
+    secCounter = 0;
+  }
   
-  if(secondCounter == 5) {
-    Serial.println("5 Seconds have passed");
-    secondCounter = 0;   
+  if((secCounter == 1) && (ledOn)) {
+    state = 0;
+    ledOn = 0;
+    ledOff = 1;
+    secCounter = 0;
   }
 }
 
+// timer compare interrupt service routine for timer 1
 ISR(TIMER1_COMPA_vect) {
-  count16++;
-  if(count16 >= 1) {
-    Serial.println("IN 16 bit ISR");
-    count16 = 0; 
-  }  
+  secondCounter++;
+  if(secondCounter >= 5) {
+    secondCounter = 0;
+    hourCounter++;
+  }
+  
+  if(hourCounter == 2) {
+      twelvehrFlag = 1;
+  }
+  
+  if(hourCounter == 8) {
+    timeEnd = 1;
+    state = 0;
+    blinkLED();
+    noInterrupts();
+  }
 }
 
+void blinkLED() {  
+  digitalWrite(ledPin, state);
+}
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  
+  noInterrupts();
+ 
+  init16bit();    // initialize timer1
+  initTimer2();     // Timer2 initialize
+  interrupts();
+}
+
+void loop() {
+ 
+ if(twelvehrFlag) {
+  if(timeEnd == 0) {
+    blinkLED();
+  }  
+ } 
+}
